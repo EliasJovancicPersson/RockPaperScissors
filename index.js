d464a79fe3b856a9ee2cae17792a5c98e6192713
Elias Jovancic Persson <Eliasjovancicpersson@gmail.com>
2022-09-07 23:13:04 +0200

    Basic game logic

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-function getComputerChoice(){
+function GetComputerChoice(){
     let randomNuber = Math.floor(Math.random() * 3);
     if(randomNuber === 0){
-        return "Rock";
+        return "rock";
     }
     else if(randomNuber === 1){
-        return "Paper";
+        return "paper";
     }
     else
     {
-        return "Scissors"
+        return "scissors"
     }
 }
+
+function PlayerSelection(){
+    let input = prompt("Rock, Paper or Scissors?").toLowerCase();
+    if(input === "rock" || input === "paper" || input === "scissors"){
+        return input;
+    }
+    else
+    {
+        console.log("Error")
+        playerSelection();
+    }
+}
+
+function CheckWinner(playerSel,computerSel){
+
+    if(playerSel === computerSel){
+        return "Tie";
+    }
+    else if(playerSel === "rock" && computerSel === "paper"){
+        return "Computer wins!";
+    }
+    else if(playerSel === "paper" && computerSel === "scissors"){
+        return "Computer wins!";
+    }
+    else if(playerSel === "scissors" && computerSel === "rock"){
+        return "Computer wins!";
+    }
+    else if(playerSel === "rock" && computerSel === "scissors"){
+        return "Player wins!";
+    }
+    else if(playerSel === "paper" && computerSel === "rock"){
+        return "Player wins!";
+    }
+    else if(playerSel === "scissors" && computerSel === "paper"){
+        return "Player wins!";
+    }
+}
+
+
+
+
+
+console.log(CheckWinner(PlayerSelection(),GetComputerChoice()));
 
 
