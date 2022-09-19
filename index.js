cf455acfe46b0327d2ef97f98a8efcd9e2c219c3
Elias Jovancic Persson <EliasJovancicPersson@gmail.com>
2022-09-20 00:54:58 +0200

    UI nearly done

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const choicesDiv = document.querySelector("#choices");
+const resultsDiv = document.querySelector("#results");
+
+
+rock.addEventListener('click', function () {
+    PlayRound("rock");
+});
+paper.addEventListener('click', function () {
+    PlayRound("paper");
+});
+scissors.addEventListener('click', function (){
+    PlayRound("scissors");
+});
+
 function GetComputerChoice(){
     let randomNuber = Math.floor(Math.random() * 3);
     if(randomNuber === 0){
@@ -34,31 +51,52 @@ function CheckWinner(playerSel,computerSel){
 
     if(playerSel === computerSel){
         console.log("Tie");
+        DisplayChoices(playerSel, "player");
+        DisplayChoices(computerSel, "computer");
         return "Tie";
     }
     else if(playerSel === "rock" && computerSel === "paper" || playerSel === "paper" && computerSel === "scissors" || playerSel === "scissors" && computerSel === "rock"){
         console.log("computer won ");
+        DisplayChoices(playerSel, "player");
+        DisplayChoices(computerSel, "computer");
         return computerScore++;
     }
     else if(playerSel === "rock" && computerSel === "scissors" || playerSel === "scissors" && computerSel === "paper" || playerSel === "paper" && computerSel === "rock"){
         console.log("Player won ");
+        DisplayChoices(playerSel, "player");
+        DisplayChoices(computerSel, "computer");
         return playerScore++;
     }
 }
 
 function CheckGameOver() {
     if (computerScore > 4) {
-        const p = document.createElement('p')
-        p.textContent = "Computer won"
-        p.setAttribute('style',"justify-self: center;align-self: center;")
-        resultsDiv.appendChild(p)
+        DisplayResults("Computer won");
     }
     else if (playerScore > 4) {
-        const p = document.createElement('p')
-        p.textContent = "Player won"
-        p.setAttribute('style',"justify-self: center;align-self: center;")
-        resultsDiv.appendChild(p)
+        DisplayResults("Player won");
     }
+}
+
+function DisplayChoices(string,id) {
+    if (document.querySelector("#"+id)) {
+        const p = document.querySelector("#"+id)
+        p.textContent = id + " " + string;
+    }
+    else {
+        const p = document.createElement('p');
+        p.setAttribute('style', "justify-self: center;align-self: center; margin: 15px;")
+        p.setAttribute('id',id)
+        p.textContent = id + " " + string;
+        choicesDiv.appendChild(p);
+    }
+}
+
+function DisplayResults(string) {
+    const p = document.createElement('p');
+    p.textContent = string;
+    p.setAttribute('style', "justify-self: center;align-self: center; margin: 15px;");
+    resultsDiv.appendChild(p);
 }
 
 function PlayRound(sel) {
@@ -66,20 +104,7 @@ function PlayRound(sel) {
     CheckGameOver();
 }
 
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissors = document.querySelector('#scissors');
-const resultsDiv = document.querySelector("#results");
 
-rock.addEventListener('click', function () {
-    PlayRound("rock");
-});
-paper.addEventListener('click', function () {
-    PlayRound("paper");
-});
-scissors.addEventListener('click', function (){
-    PlayRound("scissors");
-});
 
 
 
